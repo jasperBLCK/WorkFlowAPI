723067989ea32740f6d8a3d55c31db5ff87106a3
jasperBLCK <148188702+jasperBLCK@users.noreply.github.com>
2025-09-06 14:24:44 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres:ansar09595@localhost:5432/taskmanager"
+DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost:5432/projectname"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
